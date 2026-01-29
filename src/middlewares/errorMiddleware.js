2d03138bcfb421df9d63641fd339a1d57e2cbc6f
Ahadraza <ahadraza4533@gmail.com>
2026-01-30 00:54:43 +0530

    done

--- src/middlewares/errorMiddleware.js
+++ src/middlewares/errorMiddleware.js
@@ -6,11 +6,20 @@ export const notFound = (req, res, next) => {
 };
 
 /* --------- ERROR HANDLER --------- */
-export const errorHandler = (err, req, res, next) => {
+const errorHandler = (err, req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://appointment-frontend-livid-xi.vercel.app"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
 
   res.status(statusCode).json({
-    message: err.message,
-    stack: process.env.NODE_ENV === "production" ? null : err.stack
+    message: err.message || "Server Error",
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
   });
 };
+
+export { errorHandler };
+
